549a869f7fe80f70791031c0b0ba788fed271f6d
Alex Barnett <alexander.barnett@vanderbilt.edu>
2019-10-05 16:19:02 -0500

    Switched to recording video over individual images

--- slam_evaluation/capture_camera.py
+++ slam_evaluation/capture_camera.py
@@ -6,7 +6,8 @@ from dronekit import connect, VehicleMode
 import time
 
 GPS_NPY_FILE_BASE = "./gps_data/gps_pose"
-DATA_DIR = './camera_data/flight_0'
+CAMERA_DATA_DIR = './camera_data'
+CAMERA_VIDEO_FILE = CAMERA_DATA_DIR + '/flight_0.avi'
 
 # Set up option parsing to get connection string
 parser = argparse.ArgumentParser(description='Print out vehicle state information. Connects to SITL on local PC by default.')
@@ -42,42 +43,44 @@ while not vehicle.home_location:
     cmds.wait_ready()
     print("Waiting for home location")
 
-cap = cv2.VideoCapture(0)
 ct = 0
 gps_ct = 0
 pose = []
 start_time = time.time()
+
+# Define video capture and writer
+cap = cv2.VideoCapture(0)
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter(CAMERA_VIDEO_FILE, fourcc, 20.0, (800, 600))
+
 while True:
     # Capture image
     ret, img = cap.read()
 
-    # Display the resulting frame
-    # cv2.imshow('frame', img)
-    # if cv2.waitKey(1) & 0xFF == ord('q'):
-    #     break
+    if ret:
 
-    # Retrieve local GPS coordinates from vehicle
-    location_gps = vehicle.location.global_relative_frame
-    pose_x = location_gps.lat - vehicle.home_location.lat
-    pose_y = location_gps.lon - vehicle.home_location.lon
-    pose_z = location_gps.alt
+        # Write the image
+        out.write(img)
 
-    # To store in txt file
-    pose.append([pose_x, pose_y, pose_z])
-    # Save image
-    cv2.imwrite(DATA_DIR + '/image_{}.png'.format(ct), img)
-    ct += 1
+        # Retrieve global GPS coordinates w/ relative alt from vehicle
+        # Subtract the home location to obtain full relative coordinates
+        location_gps = vehicle.location.global_relative_frame
+        pose_x = location_gps.lat - vehicle.home_location.lat
+        pose_y = location_gps.lon - vehicle.home_location.lon
+        pose_z = location_gps.alt
+        pose.append([pose_x, pose_y, pose_z])
 
-    # Save GPS Pose every minute
-    if int(time.time() - start_time) > 2:
-        np.save(GPS_NPY_FILE_BASE + "_{}".format(gps_ct), np.array(pose))
-        pose = []
-        start_time = time.time()
-        gps_ct += 1
+        # Save GPS pose every minute
+        if int(time.time() - start_time) > 2:
+            np.save(GPS_NPY_FILE_BASE + "_{}".format(gps_ct), np.array(pose))
+            pose = []
+            start_time = time.time()
+            gps_ct += 1
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        if not vehicle.armed:
+            break
 
 # When everything done, release the capture
 cap.release()
+out.release()
 cv2.destroyAllWindows()
