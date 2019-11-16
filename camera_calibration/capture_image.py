be2e65173d685abdff98603637797bcea097bb43
Alex Barnett <alexander.barnett@vanderbilt.edu>
2019-11-16 16:49:57 -0600

    new calibration images

--- camera_calibration/capture_image.py
+++ camera_calibration/capture_image.py
@@ -1,19 +1,20 @@
 import numpy as np
 import cv2
-
+import time
 IMAGE_DIR = "./calibration_images_fisheye"
 
 # Read images from camera
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 ct = 0
 while(True):
     # Capture frame-by-frame
     ret, img = cap.read()
 
-    cv2.imshow("Image", img)
+    #cv2.imshow("Image", img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-    elif cv2.waitKey(1) & 0xFF == ord('s'):
-        cv2.imwrite(IMAGE_DIR + '/calib_image_{}.png'.format(ct), img)
-        print("Saved image {}".format(ct))
-        ct += 1
+    #elif cv2.waitKey(1) & 0xFF == ord('s'):
+    cv2.imwrite(IMAGE_DIR + '/calib_image_{}.png'.format(ct), img)
+    print("Saved image {}".format(ct))
+    ct += 1
+    time.sleep(1)
