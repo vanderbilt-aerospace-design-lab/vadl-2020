32bdab699b7f6ae693cfef36c3853ea2cc099013
Alexander Barnett <alexander.barnett@vanderbilt.edu>
2019-12-23 02:42:05 -0600

    Created Camera, VideoStream, VideoWriter, and MarkerTracker classes. Well documented

---
+++ marker_detection/camera.py
@@ -0,0 +1,138 @@
+import time
+from imutils.video import VideoStream
+from imutils.video import FileVideoStream
+from utils import cv_utils
+import cv2
+import datetime
+
+CALIBRATION_FILE = "../camera_calibration/calibration_data/arducam.yaml"
+VIDEO_DIR = "./videos"
+
+''' Camera Class
+
+    This high-level class retains camera parameters, such as resolution and frame rate. It also keeps track of 
+    if this camera is being used on a Raspberry Pi, since the Pi requires different code for retrieving images.
+'''
+class Camera(object):
+    def __init__(self,
+                 use_pi=-1,
+                 resolution=(640, 480),
+                 framerate=30):
+
+        self.resolution = resolution
+        self.framerate = framerate
+        self.use_rpi = 1 if use_pi > 0 else 0
+
+        # Load camera calibration parameters
+        self.camera_mat, self.dist_coeffs = cv_utils.load_yaml(CALIBRATION_FILE)
+        self.focal_length = self.camera_mat[1][1]
+
+    def get_resolution(self):
+        return self.resolution
+
+    def get_framerate(self):
+        return self.framerate
+
+    def get_camera_mat(self):
+        return self.camera_mat
+
+    def get_dist_coeffs(self):
+        return self.dist_coeffs
+
+    def get_focal_length(self):
+        return self.focal_length
+
+''' VideoStreamer Class
+
+    This class is used to stream video from an arbitrary camera source. Once the source is defined, all the methods 
+    are the same no matter what camera or computer you are using. It utilizes the VideoStream class, created by 
+    Adrian Rosebrock and well documented at these links:
+    
+    https://www.pyimagesearch.com/2016/01/04/unifying-picamera-and-cv2-videocapture-into-a-single-class-with-opencv/
+    https://www.pyimagesearch.com/2015/12/21/increasing-webcam-fps-with-python-and-opencv/
+    https://www.pyimagesearch.com/2015/12/28/increasing-raspberry-pi-fps-with-python-and-opencv/
+    
+    In short, it performs all camera I/O in a separate thread, which reduces I/O latency. It also standardizes methods
+    between a USB cam and a Picam.
+    
+    src: Used only for cv2.VideoCapture(src). 0 means 1st camera, 1 is 2nd camera, etc... If you are on a laptop, then
+         0 is the webcam, while 1 may be a camera plugged in by USB. On the Pi, 0 would be the Picam. src can also be a
+         video file name, passed in as a string.
+    use_pi: Make this > 0 if you are running this on the Pi.    
+    resolution: Camera resolution
+    framerate: Camera frame rate
+'''
+class VideoStreamer(Camera):
+    def __init__(self,
+                 src=0,
+                 use_pi=-1,
+                 resolution=(640, 480),
+                 framerate=30):
+
+        super(VideoStreamer, self).__init__(use_pi=use_pi,
+                                            resolution=resolution,
+                                            framerate=framerate)
+
+        # VideoStream class is used for live stream.
+        # FileVideoStream class is used for streaming from a saved video.
+        if isinstance(src, int):
+            self.vs = VideoStream(src=src,
+                                  usePiCamera=use_pi > 0,
+                                  resolution=resolution,
+                                  framerate=framerate).start()
+        else:
+            self.vs = FileVideoStream(path=src)
+
+        # Let camera warm up
+        time.sleep(1.0)
+
+    # Returns the frame as a np array
+    def read(self):
+        return self.vs.stream.read()[1]
+
+''' VideoWriter Class
+
+    This class is used to save videos. It will automatically name your file using the current date and time if you 
+    do not specify a name. Default encoding is .avi, since this has been proven to work best with cv2's funky video 
+    writing methods.
+'''
+class VideoWriter(Camera):
+    def __init__(self,
+                 video_dir="./videos",
+                 video_file=None,
+                 ext=".avi",
+                 resolution=(640, 480),
+                 framerate=30):
+
+        super(VideoWriter, self).__init__(resolution=resolution,
+                                          framerate=framerate)
+
+        self.ext = ext
+        if video_file is None:
+            video_file = self.create_file_name()
+
+        # Codec encoding. You shouldn't have to mess with this, and it is highly recommended you don't!
+        self.fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+        self.writer = cv2.VideoWriter(video_dir + "/" + video_file, self.fourcc, framerate, resolution, True)
+
+    # Generates a file name based off the current date and time.
+    def create_file_name(self):
+        date = datetime.datetime.now()
+        return str(date.year) + "_" + str(date.month) + "_" \
+               + str(date.day) + "_" + str(date.hour) + "_" + str(date.minute) + self.ext
+
+    # Write a single frame to the video output.
+    def write(self, frame):
+        self.writer.write(frame)
+
+    # Release the output when done (rarely needed - video is not corrupted if you use Ctrl-C or turn off your computer)
+    def release(self):
+        self.writer.release()
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+
---
+++ marker_detection/marker_tracker.py
@@ -0,0 +1,412 @@
+from camera import VideoStreamer, VideoWriter
+from abc import abstractmethod
+import cv2
+import cv2.aruco as aruco
+import numpy as np
+
+''' Marker Tracker Classes
+
+    These classes are used to track a marker using a single camera. They maintain various image and marker data, and
+    can be used to retrieve the pose relative to the camera. Each class is used to detect a different kind of marker.
+    The classes are derived from the VideoStreamer class, which is used to retrieve images from a camera or video file 
+    in a parallel thread.'''
+
+# Abstract Base Class. Each marker tracker needs to define marker length, pose, the current frame,
+# and some variables used for visualization.
+class MarkerTracker(VideoStreamer):
+    def __init__(self,
+                 src=0,
+                 use_pi=-1,
+                 resolution=(640, 480),
+                 framerate=30,
+                 marker_length=0.159,
+                 debug=0):
+
+        super(MarkerTracker, self).__init__(src=src,
+                                            use_pi=use_pi,
+                                            resolution=resolution,
+                                            framerate=framerate)
+        self.marker_length = marker_length
+        self.pose = None
+        self.marker_found = False
+        self.cur_frame = None
+        self.detected_frame = None # Store visual information about the marker
+        self.debug = debug
+
+        # Save video
+        if self.debug:
+            self.video_writer = VideoWriter(resolution=self.get_resolution(), framerate=self.get_framerate())
+
+    def is_marker_found(self):
+        return self.marker_found
+
+    def get_marker_length(self):
+        return self.marker_length
+
+    def get_pose(self):
+        return self.pose
+
+    def get_debug(self):
+        return self.debug
+
+    def set_pose(self, pose):
+        self.pose = pose
+
+    # This is where the marker tracking happens. Various image processing is performed, and if the marker is detected,
+    # the pose will be updated and a boolean will be returned.
+    @abstractmethod
+    def track_marker(self, alt):
+        pass
+
+    # Used for debugging or recording purposes. Will display images to your display and save a video.
+    @abstractmethod
+    def visualize(self):
+        pass
+
+# Tracks a yellow marker.
+class ColorMarkerTracker(MarkerTracker):
+    def __init__(self,
+                 src=0,
+                 use_pi=-1,
+                 resolution=(640, 480),
+                 framerate=30,
+                 marker_length=3.048,
+                 debug=0):
+
+        super(ColorMarkerTracker, self).__init__(src=src,
+                                                 use_pi=use_pi,
+                                                 resolution=resolution,
+                                                 framerate=framerate,
+                                                 marker_length=marker_length,
+                                                 debug=debug)
+
+        # Below the altitude threshold, dynamic Otsu thresholding is performed on the lightness spectrum
+        # in the LAB color space. Above the threshold, binary thresholding is performed. This was determined to be the
+        # best approach for detecting the yellow marker in a large altitude range. The altitude threshold should be
+        # empirically determined.
+        self.alt_thresh = 25
+
+        # Stores each image processing step
+        self.undistort_frame = None
+        self.lab_space_frame = None
+        self.thresh_yellow_frame = None
+        self.thresh_light_frame = None
+        self.thresh_sum_frame = None
+        self.opening_frame = None
+        self.processed_frame = None # Final thresholded frame
+
+    # For debugging and recording. If on the Pi, we only want to save videos, since the GUI may not be available.
+    def visualize(self):
+        if not self.marker_found:
+            self.detected_frame = self.cur_frame
+
+        if self.use_rpi:
+
+            self.video_writer.write(self.detected_frame)
+        else:
+            # Reduce all 9 image dimensions so they can be merged together
+            reduced_dim = (self.resolution[0] / 3, self.resolution[1] / 3)
+
+            # Convert the mask images into three-channel images so they can be merged with the rest
+            thresh_yellow_frame_three = cv2.merge((self.thresh_yellow_frame,self.thresh_yellow_frame,self.thresh_yellow_frame))
+            thresh_light_frame_three = cv2.merge((self.thresh_light_frame,self.thresh_light_frame,self.thresh_light_frame))
+            thresh_sum_frame_three = cv2.merge((self.thresh_sum_frame,self.thresh_sum_frame,self.thresh_sum_frame))
+            opening_frame_three = cv2.merge((self.opening_frame,self.opening_frame,self.opening_frame))
+            processed_three = cv2.merge((self.processed_frame,self.processed_frame,self.processed_frame))
+
+            # Resize
+            img1 = cv2.resize(self.read(), reduced_dim, interpolation=cv2.INTER_AREA)
+            img2 = cv2.resize(self.undistort_frame, reduced_dim, interpolation=cv2.INTER_AREA)
+            img3 = cv2.resize(self.lab_space_frame, reduced_dim, interpolation=cv2.INTER_AREA)
+            img4 = cv2.resize(thresh_yellow_frame_three, reduced_dim, interpolation=cv2.INTER_AREA)
+            img5 = cv2.resize(thresh_light_frame_three, reduced_dim, interpolation=cv2.INTER_AREA)
+            img6 = cv2.resize(thresh_sum_frame_three, reduced_dim, interpolation=cv2.INTER_AREA)
+            img7 = cv2.resize(opening_frame_three, reduced_dim, interpolation=cv2.INTER_AREA)
+            img8 = cv2.resize(processed_three, reduced_dim, interpolation=cv2.INTER_AREA)
+            img9 = cv2.resize(self.detected_frame, reduced_dim, interpolation=cv2.INTER_AREA)
+
+            # Merge into one image
+            row1 = np.concatenate((img1, img2, img3), axis=1)
+            row2 = np.concatenate((img4, img5, img6), axis=1)
+            row3 = np.concatenate((img7, img8, img9), axis=1)
+            all_frames = np.concatenate((row1, row2, row3), axis=0)
+
+            # Deal with any rounding errors
+            if (all_frames.shape[0], all_frames.shape[1]) != self.resolution:
+                all_frames = cv2.resize(all_frames,self.resolution, interpolation=cv2.INTER_AREA)
+
+            # Display
+            cv2.namedWindow("Original, Undistorted, Lab // "
+                       "Yellow Thresh, Light Thresh, Sum Thresh // "
+                       "opening_frame, Closing, Final", cv2.WINDOW_FULLSCREEN)
+            cv2.imshow("Original, Undistorted, Lab // "
+                       "Yellow Thresh, Light Thresh, Sum Thresh // "
+                       "opening_frame, Closing, Final", all_frames)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                cv2.destroyAllWindows()
+
+            # Save video
+            self.video_writer.write(all_frames)
+
+    # Draw the marker corners and pose info on the image for debugging.
+    def visualize_marker_pose(self, extLeft, extRight, extTop, extBot, cX, cY, depth, c):
+        self.detected_frame = self.cur_frame
+
+        # Draw extreme points on image
+        cv2.circle(self.detected_frame, extLeft, 8, (255, 0, 0), -1)
+        cv2.circle(self.detected_frame, extRight, 8, (255, 0, 0), -1)
+        cv2.circle(self.detected_frame, extTop, 8, (255, 0, 255), -1)
+        cv2.circle(self.detected_frame, extBot, 8, (0, 0, 255), -1)
+
+        # Draw center
+        cv2.circle(self.detected_frame, (cX, cY), 7, (255, 255, 255), -1)
+        cv2.putText(self.detected_frame, "center", (cX + 20, cY + 20),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+
+        # Draw depth
+        cv2.putText(self.detected_frame, "Distance: {}".format(np.around(depth, 1)), (cX - 40, cY - 20),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+
+        # Draw XY error
+        cv2.putText(self.detected_frame, "XY Error: {}, {}".format(np.around(self.pose[0], 1), np.around(self.pose[1], 1)), (cX - 40, cY - 40),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+
+        # Draw contours on image
+        cv2.drawContours(self.detected_frame, [c], 0, (0, 255, 0), 3)
+
+    # This method tracks the color yellow. First, the image is undistorted to account for any lens distortion. Next,
+    # it is converted from BGR to LAB color space. The LAB color space is great for color detection since it separates
+    # the image into lightness (L), green -> red (*a), and blue -> yellow (*b). Various thresholds (explained within)
+    # are applied to identify the yellow, and the thresholds are further refined to eliminate noise.
+    #
+    # After color detection, we search for a rectangle shape using contour approximation. This rules out all noise
+    # left over. The pose is calculated and scaled using the known marker length.
+    def track_marker(self, alt=25):
+        self.cur_frame = self.read()
+
+        # Undistort image to get rid of fisheye distortion
+        self.undistort_frame = cv2.undistort(self.cur_frame, self.camera_mat, self.dist_coeffs)
+
+        # Convert to LAB color space
+        # LAB color space is used to separate lightness from color attributes
+        # L is dark (0) to light (255)
+        # A is Green (0) to Red (255)
+        # B is Blue (0) to Yellow (255)
+        self.lab_space_frame = cv2.cvtColor(self.undistort_frame, cv2.COLOR_BGR2Lab)
+
+        # Blur the image to reduce noise
+        self.lab_space_frame = cv2.GaussianBlur(self.lab_space_frame, (5,5), 0)
+
+        # Lightness is thresholded since the yellow tarp will most likely be the lightest object in the image.
+        # At lower altitudes, the marker takes up the majority of the image, and OTSU normalization is ideal, since
+        # it dynamically chooses the threshold value to segment between "light" and "not light". At higher altitudes,
+        # the marker is too small for OTSU to threshold well, so a binary threshold is used.
+        if alt < self.alt_thresh:
+            # Dynamically threshold lightness
+            retval2, self.thresh_light_frame = cv2.threshold(self.lab_space_frame[:, :, 0], 200, 255,
+                                                             cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+        else:
+            # Binary threshold lightness
+            retval2, self.thresh_light_frame = cv2.threshold(self.lab_space_frame[:, :, 0], 200, 255,
+                                                             cv2.THRESH_BINARY)
+
+        # Threshold yellow; Everything from 0 to 127 in the B space is made 0 - these are blueish colors
+        retval1, self.thresh_yellow_frame = cv2.threshold(self.lab_space_frame[:, :, 2], 127, 255,
+                                                          cv2.THRESH_BINARY)
+
+        # Combine the yellow and lightness thresholds, letting through only the pixels that are white in each image
+        self.thresh_sum_frame = cv2.bitwise_and(self.thresh_yellow_frame, self.thresh_light_frame)
+
+        # Create kernels for dilution and erosion operations; larger ksize means larger pixel neighborhood where the
+        # operation is taking place
+        se1 = cv2.getStructuringElement(cv2.MORPH_RECT, ksize=(8, 8))
+        se2 = cv2.getStructuringElement(cv2.MORPH_RECT, ksize=(50, 50))
+
+        # Perform "opening_frame." This is erosion followed by dilation, which reduces noise. The ksize is pretty small,
+        # otherwise the white in the marker is eliminated
+        self.opening_frame = cv2.morphologyEx(self.thresh_sum_frame, cv2.MORPH_OPEN, se1)
+
+        # Perform "closing." This is dilution followed by erosion, which fills in black gaps within the marker. This is
+        # necessary if the lightness threshold is not able to get the entire marker at lower altitudes
+        self.processed_frame = cv2.morphologyEx(self.opening_frame, cv2.MORPH_CLOSE, se2)
+
+        # Find contours
+        # TODO: The return values here are dependent on Opencv 2,3,or 4, so either be sure of which version
+        #  we are using or add some code to check the version
+        _, contours, hierarchy = cv2.findContours(self.processed_frame, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+        # Find corners of the contour
+        if len(contours) >= 1:
+
+            # Take contour w/ max area; the marker will always be the largest contour in the image
+            c = max(contours, key=cv2.contourArea)
+
+            # Approximate the contour
+            peri = cv2.arcLength(c, True)
+            approx = cv2.approxPolyDP(c, 0.03 * peri, True)
+            # Keep going if the contour is a square
+            if self.is_rectangle(approx):
+                c = approx
+
+                # Find extreme points
+                extLeft = tuple(c[c[:, :, 0].argmin()][0])
+                extRight = tuple(c[c[:, :, 0].argmax()][0])
+                extTop = tuple(c[c[:, :, 1].argmin()][0])
+                extBot = tuple(c[c[:, :, 1].argmax()][0])
+
+                # Find the contour center
+                M = cv2.moments(c)
+                cX = int(M["m10"] / M["m00"])
+                cY = int(M["m01"] / M["m00"])
+
+                # Find the distance to the platform
+                # TODO: This will most likely be replaced with depth from the Realsense camera
+                pixel_length = peri / 4
+                depth = (self.focal_length / pixel_length) * self.marker_length
+
+                # Determine the *approximate* scale factor
+                scale_factor = self.marker_length / pixel_length
+
+                # Find positional error from center of platform; Pixels
+                err_x = cX - (self.get_resolution()[0] / 2)
+                err_y = (self.get_resolution()[1] / 2) - cY
+
+                # Convert to meters
+                err_x *= scale_factor
+                err_y *= scale_factor
+
+                self.set_pose(np.array([err_x, err_y, 0]))
+
+                # Debug
+                self.marker_found = True
+                if self.get_debug():
+                    self.visualize_marker_pose(extLeft, extRight, extTop, extBot,
+                                                               cX, cY, depth, c)
+                    self.visualize()
+
+                return True
+
+        # Debug
+        self.marker_found = False
+        if self.get_debug():
+            self.visualize()
+
+        return False
+
+    # Checks if the contour approximation is a rectangle.
+    def is_rectangle(self, approx):
+        if len(approx) == 4:
+
+            # compute the bounding box of the contour and use the
+            # bounding box to compute the aspect ratio
+            (x, y, w, h) = cv2.boundingRect(approx)
+            ar = w / float(h)
+
+            # a rectangle will have an aspect ratio that is within this range
+            return True if 0.75 <= ar <= 1.25 else False
+        return False
+
+    def get_detected_frame(self):
+        return self.detected_frame
+
+# Tracks an Aruco marker.
+class ArucoTracker(MarkerTracker):
+    def __init__(self,
+                 src=0,
+                 use_pi=-1,
+                 resolution=(640, 480),
+                 framerate=30,
+                 marker_length=0.159,
+                 debug=0):
+
+        super(ArucoTracker, self).__init__(src=src,
+                                          use_pi=use_pi,
+                                          resolution=resolution,
+                                          framerate=framerate,
+                                          marker_length=marker_length,
+                                          debug=debug)
+
+        # Create aruco dictionary
+        self.aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
+        self.aruco_param = aruco.DetectorParameters_create()  # Default parameters
+
+        self.rvec = None
+
+    # Visualize the Aruco marker detection. If on the Pi, only saves a video, since the GUI may not be available.
+    def visualize(self):
+        if not self.marker_found:
+            self.detected_frame = self.cur_frame
+
+        self.video_writer.write(self.detected_frame)
+
+        if not self.use_rpi:
+            cv2.imshow("Aruco Tracker", self.detected_frame)
+            print(self.pose)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                cv2.destroyAllWindows()
+
+    # The Aruco marker is tracked here. The pose is saved if the marker is found.
+    # Returns True if found, else False.
+    def track_marker(self, alt=0):
+        # Find the marker
+        self.cur_frame = self.read()
+
+        corners, ids, rejectedImgPoints = aruco.detectMarkers(self.cur_frame, self.aruco_dict,
+                                                              parameters=self.aruco_param)
+
+        # If a marker is found, estimate the pose
+        if ids is not None:
+            corners = corners[0]  # Corners are given clockwise starting at top left
+            self.rvec, self.pose = aruco.estimatePoseSingleMarkers(corners, self.marker_length,
+                                                                   self.camera_mat, self.dist_coeffs)
+
+            # Reduce extra array dimensions
+            self.rvec = self.rvec[0][0]
+            self.pose = self.pose[0][0]
+
+            # Debug
+            self.marker_found = True
+            if self.get_debug():
+                self.detected_frame = aruco.drawAxis(self.cur_frame, self.camera_mat, self.dist_coeffs,
+                                                     self.rvec, self.pose, 0.1)
+                self.visualize()
+
+            return True
+
+        # Debug
+        self.marker_found = False
+        if self.get_debug():
+            self.visualize()
+
+        return False
+
+    # Use this function to create and save an Aruco marker if you do not already have one.
+    def create_and_save_marker(self):
+
+        # Second param is ID - change if you want a different marker
+        # Third param is border bits - leave it large
+        marker_img = aruco.drawMarker(self.aruco_dict, 20, 600)
+
+        cv2.imshow("Aruco axes", marker_img)
+        k = cv2.waitKey(0)
+        if k == 27:  # wait for ESC key to exit
+            cv2.destroyAllWindows()
+        elif k == ord('s'):  # wait for 's' key to save and exit
+            cv2.imwrite('aruco_marker.png', marker_img)
+            cv2.destroyAllWindows()
+
+    def get_marker_rotation(self):
+        return self.rvec
+
+def main():
+    mt = ArucoTracker(src=0, debug=1)
+
+    while True:
+        mt.track_marker()
+        if mt.is_marker_found():
+            print(mt.get_pose())
+
+if __name__ == "__main__":
+    main()
+
+
