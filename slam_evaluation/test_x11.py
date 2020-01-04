07cdfa7f8d5a25ea40eb8c2b87192310bf52e1ef
Alexander Barnett <alexander.barnett@vanderbilt.edu>
2020-01-03 18:26:27 -0600

    test that X11 forwarding works

--- slam_evaluation/test_x11.py
+++ slam_evaluation/test_x11.py
@@ -1,14 +1,18 @@
 import cv2
 
-cap = cv2.VideoWriter(0)
-while True:
-    ret, frame = cap.read()
+def main():
+    cap = cv2.VideoCapture(0)
+    while True:
+        ret, frame = cap.read()
 
-    # Display the resulting frame
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        # Display the resulting frame
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__=="__main__":
+    main()
