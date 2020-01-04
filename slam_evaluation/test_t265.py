858c093c060124d48368669c58982bb78ca760ef
Alexander Barnett <alexander.barnett@vanderbilt.edu>
2020-01-04 12:54:24 -0600

    test script for rs pose

---
+++ slam_evaluation/test_t265.py
@@ -0,0 +1,90 @@
+from __future__ import print_function
+import os
+import argparse
+import numpy as np
+from dronekit import VehicleMode
+import time
+import transformations as tf
+from utils import dronekit_utils
+
+# Set MAVLink protocol to 2.
+os.environ["MAVLINK20"] = "1"
+
+# Import the libraries
+import pyrealsense2 as rs
+
+FREQ = 30 # Hz
+
+def realsense_connect():
+    print("Connecting to Realsense")
+
+    # Declare RealSense pipeline, encapsulating the actual device and sensors
+    pipe = rs.pipeline()
+
+    # Build config object before requesting data
+    cfg = rs.config()
+
+    # Enable the stream we are interested in
+    cfg.enable_stream(rs.stream.pose)  # Positional data
+
+    # Start streaming with requested config
+    pipe.start(cfg)
+
+    return pipe
+
+def rs_to_body(data):
+
+    # Downward facing
+    H_T265body_aeroBody = np.array([[0,1,0,0],[1,0,0,0],[0,0,-1,0],[0,0,0,1]])
+
+    # Forward facing
+    #  H_aeroRef_T265Ref = np.array([[0, 0, -1, 0], [1, 0, 0, 0], [0, -1, 0, 0], [0, 0, 0, 1]])
+    #  H_T265body_aeroBody = np.linalg.inv(H_aeroRef_T265Ref)
+
+    rotation_euler = tf.euler_from_quaternion([data.rotation.w, data.rotation.x, data.rotation.y, data.rotation.z])
+    rot_mat_x = tf.rotation_matrix(rotation_euler[0], (1, 0, 0))
+    rot_mat_y = tf.rotation_matrix(rotation_euler[1], (0, 1, 0))
+    rot_mat_z = tf.rotation_matrix(rotation_euler[2], (0, 0, 1))
+    transform = np.matmul(np.matmul(rot_mat_x, rot_mat_y), rot_mat_z)
+
+    pose = np.array([data.translation.x, data.translation.y, data.translation.z, 1])
+    return np.matmul(np.matmul(pose, transform), H_T265body_aeroBody)
+
+
+def test_rs(pipe):
+
+    print("Recording data...")
+    while True:
+        frame_time = time.time()
+
+        # Wait for frames
+        frames = pipe.wait_for_frames()
+
+        # Get pose frame
+        data = frames.get_pose_frame()
+        if data:
+
+            # Pose data consists of translation and rotation
+            rs_pose = data.get_pose_data()
+
+            # Transform RS frame to body frame
+            rs_pose_body_frame = rs_to_body(rs_pose)
+
+            print(rs_pose_body_frame)
+
+
+
+        # Sleep to preserve frequency
+        time.sleep((1.0 / FREQ) - time.time() + frame_time)
+
+
+def main():
+    # Connect to the realsense
+    pipe = realsense_connect()
+
+    # Record GPS and realsense data
+    test_rs(pipe)
+
+
+if __name__ == "__main__":
+    main()
