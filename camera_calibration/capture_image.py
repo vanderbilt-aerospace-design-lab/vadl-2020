492a508d3e95086b562ec6d2a103427213ac74c1
Alexander Barnett <alexander.barnett@vanderbilt.edu>
2020-01-11 19:10:08 -0600

    made this script Pi/laptop agnostic

--- camera_calibration/capture_image.py
+++ camera_calibration/capture_image.py
@@ -1,20 +1,32 @@
-import numpy as np
 import cv2
-import time
-IMAGE_DIR = "./calibration_images_arducam"
+from marker_detection.camera import VideoStreamer
+import argparse
 
-# Read images from camera
-cap = cv2.VideoCapture(0)
-ct = 0
-while(True):
-    # Capture frame-by-frame
-    ret, img = cap.read()
+IMAGE_DIR = "/media/usb/calibration_images_arducam"
 
-    #cv2.imshow("Image", img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-    #elif cv2.waitKey(1) & 0xFF == ord('s'):
-    cv2.imwrite(IMAGE_DIR + '/calib_image_{}.jpg'.format(ct), img)
-    print("Saved image {}".format(ct))
-    ct += 1
-    time.sleep(1)
+#Set up option parsing to get connection string
+parser = argparse.ArgumentParser(description='Capture images on a webcam or Pi.')
+parser.add_argument('-d','--display', default=0,
+                    help="Display camera stream. 1 = Yes, 0 = No")
+args = vars(parser.parse_args())
+
+def main():
+    ct = 0
+    vs = VideoStreamer(use_pi=1)
+    while True:
+        frame = vs.read()
+
+        if args["display"]:
+            cv2.imshow("Image", frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        elif cv2.waitKey(1) & 0xFF == ord('s'):
+            cv2.imwrite(IMAGE_DIR + '/calib_image_{}.jpg'.format(ct), frame)
+            print("Saved image {}".format(ct))
+            ct += 1
+
+    # When everything done, release the capture
+    vs.stop()
+
+if __name__=="__main__":
+    main()
