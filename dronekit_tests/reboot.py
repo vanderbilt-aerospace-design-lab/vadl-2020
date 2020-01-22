5e86343b84eb27e29d98804ec56646c9a75e09a8
Alexander Barnett <alexander.barnett@vanderbilt.edu>
2020-01-21 20:16:24 -0600

    test reboot and reconnection to Pixhawk

---
+++ dronekit_tests/reboot.py
@@ -0,0 +1,28 @@
+from dronekit import connect, VehicleMode
+import time
+import dronekit_sitl
+import argparse
+from utils import dronekit_utils
+
+''' Test script for auto reboot and reconnection'''
+
+#Set up option parsing to get connection string
+parser = argparse.ArgumentParser(description='Control Copter and send commands in GUIDED mode ')
+parser.add_argument('--sitl',
+                   help="Vehicle connection target string. If specified, SITL will be used.")
+args = vars(parser.parse_args())
+
+def main():
+
+    # Connect to vehicle
+    vehicle = dronekit_utils.connect_vehicle_args(args)
+
+    # Reboot
+    dronekit_utils.reboot(vehicle)
+
+    # Reconnect
+    dronekit_utils.connect_vehicle_args(args)
+
+
+if __name__ == "__main__":
+    main()
