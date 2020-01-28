a240bda3f1ad8c53a64bc326bac36032bdd2c44c
Alexander Barnett <alexander.barnett@vanderbilt.edu>
2020-01-27 21:36:05 -0600

    FINALLY fixed pose transform for comp. UAV Realsense configuration

--- slam_evaluation/tests/t265_to_mavlink.py
+++ slam_evaluation/tests/t265_to_mavlink.py
@@ -125,15 +125,47 @@ else:
     np.set_printoptions(precision=4, suppress=True) # Format output on terminal
     print("INFO: Debug messages enabled.")
 
-# Sideways, USB port facing the back, 45 degrees roll
-H_aeroRef_T265Ref = np.array([[-1,0,0,0],
-                              [0,0,-1,0],
-                              [0,-1,0,0],
-                              [0,0,0,1]])
-H_T265body_aeroBody = np.array([[-1,0,0,0],
-                                [0,np.sin(45*(np.pi/180)),-np.cos(45*(np.pi/180)),0],
-                                [0,-np.cos(45*(np.pi/180)),-np.sin(45*(np.pi/180)),0],
-                                [0,0,0,1]])
+''' Realsense to NED pose transform
+0: NED Origin
+1: RS Origin
+2: NED Frame
+3: RS Frame
+
+H2_0: NED Frame rel. to NED origin (pose sent to Pixhawk)
+H0_1: RS Origin rel. to NED Origin
+H1_3: RS Frame rel. to RS Origin (pose received from Realsense)
+H3_2: NED Frame rel. to RS Frame
+
+H0_2 = H0_1.dot(H1_3).dot(H3_2)
+
+H3_2 = H3_A.dot(HA_B).dot(HB_2)
+Broken up into a 180 deg. flip about z (A), 45 deg rotation about x (B) and 
+left hand to right hand coord. system change.
+'''
+
+H0_1 = np.array([[1, 0, 0, 0],
+                 [0, 0, 1, 0],
+                 [0, -1, 0, 0],
+                 [0, 0, 0, 1]])
+
+HA_3 = np.array([[-1, 0, 0, 0],
+                 [0, -1, 0, 0],
+                 [0, 0, 1, 0],
+                 [0, 0, 0, 1]])
+H3_A = tf.inverse_matrix(HA_3)
+
+HA_B = np.array([[1, 0, 0, 0],
+                 [0, np.cos(40*np.pi / 180), -np.sin(40*np.pi / 180), 0],
+                 [0, np.sin(40*np.pi / 180), np.cos(40*np.pi / 180), 0],
+                 [0, 0, 0, 1]])
+
+HB_2 = np.array([[1, 0, 0, 0],
+                 [0, 0, -1, 0],
+                 [0, 1, 0, 0],
+                 [0, 0, 0, 1]])
+
+H3_2 = H3_A.dot(HA_B).dot(HB_2)
+
 
 #######################################
 # Functions
@@ -254,9 +286,9 @@ print("INFO: Connecting to vehicle.")
 vehicle = dronekit_utils.connect_vehicle()
 print("INFO: Vehicle connected.")
 
-print("INFO: Tricking compass.")
-vehicle = trick_compass(vehicle)
-print("INFO: Compass tricked.")
+# print("INFO: Tricking compass.")
+#vehicle = trick_compass(vehicle)
+# print("INFO: Compass tricked.")
 
 # Listen to the mavlink messages that will be used as trigger to set EKF home automatically
 vehicle.add_message_listener('STATUSTEXT', statustext_callback)
@@ -299,13 +331,14 @@ try:
             current_time = int(round(time.time() * 1000000))
 
             # In transformations, Quaternions w+ix+jy+kz are represented as [w, x, y, z]!
-            H_T265Ref_T265body = tf.quaternion_matrix([data.rotation.w, data.rotation.x, data.rotation.y, data.rotation.z]) 
-            H_T265Ref_T265body[0][3] = data.translation.x * scale_factor
-            H_T265Ref_T265body[1][3] = data.translation.y * scale_factor
-            H_T265Ref_T265body[2][3] = data.translation.z * scale_factor
+            H1_3 = tf.quaternion_matrix(
+                [data.rotation.w, data.rotation.x, data.rotation.y, data.rotation.z])
+            H1_3[0][3] = data.translation.x * scale_factor
+            H1_3[1][3] = data.translation.y * scale_factor
+            H1_3[2][3] = data.translation.z * scale_factor
 
             # Transform to aeronautic coordinates (body AND reference frame!)
-            H_aeroRef_aeroBody = H_aeroRef_T265Ref.dot( H_T265Ref_T265body.dot( H_T265body_aeroBody))
+            H0_2 = (H0_1.dot(H1_3)).dot(H3_2)
 
             # Take offsets from body's center of gravity (or IMU) to camera's origin into account
             if body_offset_enabled == 1:
@@ -314,19 +347,18 @@ try:
                 H_body_camera[1][3] = body_offset_y
                 H_body_camera[2][3] = body_offset_z
                 H_camera_body = np.linalg.inv(H_body_camera)
-                H_aeroRef_aeroBody = H_body_camera.dot(H_aeroRef_aeroBody.dot(H_camera_body))
+                H0_2 = H_body_camera.dot(H0_2.dot(H_camera_body))
 
-            # Realign heading to face north using initial compass data
-            if compass_enabled == 1:
-                H_aeroRef_aeroBody = H_aeroRef_aeroBody.dot( tf.euler_matrix(0, 0, heading_north_yaw, 'sxyz'))
-             
             # Show debug messages here
             if debug_enable == 1:
-                os.system('clear') # This helps in displaying the messages to be more readable
-                print("DEBUG: Raw RPY[deg]: {}".format( np.array( tf.euler_from_matrix( H_T265Ref_T265body, 'sxyz')) * 180 / m.pi))
-                print("DEBUG: NED RPY[deg]: {}".format( np.array( tf.euler_from_matrix( H_aeroRef_aeroBody, 'sxyz')) * 180 / m.pi))
-                print("DEBUG: Raw pos xyz : {}".format( np.array( [data.translation.x, data.translation.y, data.translation.z])))
-                print("DEBUG: NED pos xyz : {}".format( np.array( tf.translation_from_matrix( H_aeroRef_aeroBody))))
+                os.system('clear')  # This helps in displaying the messages to be more readable
+                print("DEBUG: Raw RPY[deg]: {}".format(
+                    np.array(tf.euler_from_matrix(H1_3, 'sxyz')) * 180 / m.pi))
+                print("DEBUG: NED RPY[deg]: {}".format(
+                    np.array(tf.euler_from_matrix(H0_2, 'sxyz')) * 180 / m.pi))
+                print("DEBUG: Raw pos xyz : {}".format(
+                    np.array([data.translation.x, data.translation.y, data.translation.z])))
+                print("DEBUG: NED pos xyz : {}".format(np.array(tf.translation_from_matrix(H0_2))))
                 
 except KeyboardInterrupt:
     print("INFO: KeyboardInterrupt has been caught. Cleaning up...")     
--- slam_evaluation/tests/t265_to_ned.py
+++ slam_evaluation/tests/t265_to_ned.py
@@ -43,20 +43,46 @@ parser.add_argument('--debug_enable', type=int,
 args = parser.parse_args()
 debug_enable = args.debug_enable
 
-# Sideways, USB port facing the back, 45 degrees roll
-H_aeroRef_T265Ref = np.array([[-1, 0, 0, 0],
-                              [0, 0, 1, 0],
-                              [0, 1, 0, 0],
-                              [0, 0, 0, 1]])
-angle_x = 40
-angle_z = 180
-H_T265body_aeroBody = tf.quaternion_about_axis(angle_x, (1, 0, 0))
-#H_T265body_aeroBody = tf.rotation_matrix(angle_x, (-1, 0, 0))
-print(H_T265body_aeroBody)
-#H_T265body_aeroBody = np.array([[1, 0, 0, 0],
- #                               [0, -np.sin(40 * (np.pi / 180)), np.cos(40 * (np.pi / 180)), 0],
-  #                              [0, -np.cos(40 * (np.pi / 180)), -np.sin(40 * (np.pi / 180)), 0],
-   #                             [0, 0, 0, 1]])
+''' Realsense to NED pose transform
+0: NED Origin
+1: RS Origin
+2: NED Frame
+3: RS Frame
+
+H2_0: NED Frame rel. to NED origin (pose sent to Pixhawk)
+H0_1: RS Origin rel. to NED Origin
+H1_3: RS Frame rel. to RS Origin (pose received from Realsense)
+H3_2: NED Frame rel. to RS Frame
+
+H0_2 = H0_1.dot(H1_3).dot(H3_2)
+
+H3_2 = H3_A.dot(HA_B).dot(HB_2)
+Broken up into a 180 deg. flip about z (A), 45 deg rotation about x (B) and 
+left hand to right hand coord. system change.
+'''
+
+H0_1 = np.array([[1, 0, 0, 0],
+                 [0, 0, 1, 0],
+                 [0, -1, 0, 0],
+                 [0, 0, 0, 1]])
+
+HA_3 = np.array([[-1, 0, 0, 0],
+                 [0, -1, 0, 0],
+                 [0, 0, 1, 0],
+                 [0, 0, 0, 1]])
+H3_A = tf.inverse_matrix(HA_3)
+
+HA_B = np.array([[1, 0, 0, 0],
+                 [0, np.cos(40*np.pi / 180), -np.sin(40*np.pi / 180), 0],
+                 [0, np.sin(40*np.pi / 180), np.cos(40*np.pi / 180), 0],
+                 [0, 0, 0, 1]])
+
+HB_2 = np.array([[1, 0, 0, 0],
+                 [0, 0, -1, 0],
+                 [0, 1, 0, 0],
+                 [0, 0, 0, 1]])
+
+H3_2 = H3_A.dot(HA_B).dot(HB_2)
 
 if not debug_enable:
     debug_enable = 0
@@ -82,15 +108,14 @@ try:
             current_time = int(round(time.time() * 1000000))
 
             # In transformations, Quaternions w+ix+jy+kz are represented as [w, x, y, z]!
-            H_T265Ref_T265body = tf.quaternion_matrix(
+            H1_3 = tf.quaternion_matrix(
                 [data.rotation.w, data.rotation.x, data.rotation.y, data.rotation.z])
-            H_T265Ref_T265body[0][3] = data.translation.x * scale_factor
-            H_T265Ref_T265body[1][3] = data.translation.y * scale_factor
-            H_T265Ref_T265body[2][3] = data.translation.z * scale_factor
+            H1_3[0][3] = data.translation.x * scale_factor
+            H1_3[1][3] = data.translation.y * scale_factor
+            H1_3[2][3] = data.translation.z * scale_factor
 
             # Transform to aeronautic coordinates (body AND reference frame!)
-           # H_aeroRef_aeroBody = H_aeroRef_T265Ref.dot(H_T265Ref_T265body.dot(H_T265body_aeroBody))
-            H_aeroRef_aeroBody = H_T265Ref_T265body.dot(H_T265body_aeroBody)
+            H0_2 = (H0_1.dot(H1_3)).dot(H3_2)
 
             # Take offsets from body's center of gravity (or IMU) to camera's origin into account
             if body_offset_enabled == 1:
@@ -99,18 +124,18 @@ try:
                 H_body_camera[1][3] = body_offset_y
                 H_body_camera[2][3] = body_offset_z
                 H_camera_body = np.linalg.inv(H_body_camera)
-                H_aeroRef_aeroBody = H_body_camera.dot(H_aeroRef_aeroBody.dot(H_camera_body))
+                H0_2 = H_body_camera.dot(H0_2.dot(H_camera_body))
 
             # Show debug messages here
             if debug_enable == 1:
                 os.system('clear')  # This helps in displaying the messages to be more readable
                 print("DEBUG: Raw RPY[deg]: {}".format(
-                    np.array(tf.euler_from_matrix(H_T265Ref_T265body, 'sxyz')) * 180 / m.pi))
+                    np.array(tf.euler_from_matrix(H1_3, 'sxyz')) * 180 / m.pi))
                 print("DEBUG: NED RPY[deg]: {}".format(
-                    np.array(tf.euler_from_matrix(H_aeroRef_aeroBody, 'sxyz')) * 180 / m.pi))
+                    np.array(tf.euler_from_matrix(H0_2, 'sxyz')) * 180 / m.pi))
                 print("DEBUG: Raw pos xyz : {}".format(
                     np.array([data.translation.x, data.translation.y, data.translation.z])))
-                print("DEBUG: NED pos xyz : {}".format(np.array(tf.translation_from_matrix(H_aeroRef_aeroBody))))
+                print("DEBUG: NED pos xyz : {}".format(np.array(tf.translation_from_matrix(H0_2))))
 
 except KeyboardInterrupt:
     print("INFO: KeyboardInterrupt has been caught. Cleaning up...")
