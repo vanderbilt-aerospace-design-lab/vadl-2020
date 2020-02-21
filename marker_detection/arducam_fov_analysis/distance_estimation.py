7d96003b32cf286d985c564d7fd6d10f380fba13
Jake <jacob.gloudemans@vanderbilt.edu>
2020-02-21 02:21:45 -0500

    More mods to distance_estimation

--- marker_detection/arducam_fov_analysis/distance_estimation.py
+++ marker_detection/arducam_fov_analysis/distance_estimation.py
@@ -27,7 +27,6 @@ def calc_gnd_pt_offset(roll, pitch, ppd=PIX_PER_DEG):
 
     # Find center of hemisphere in pixels
     diam = ppd * 180.0
-    center = [diam / 2.0, diam / 2.0]
 
     # Precompute some values
     t = diam / 2.0
@@ -40,16 +39,17 @@ def calc_gnd_pt_offset(roll, pitch, ppd=PIX_PER_DEG):
     x = sqrt(x_sq)
     vw = VideoWriter(video_dir="/home/pi/pi_videos/",
                      video_file="drone_pi_" + str(int(time.time())),
-                     resolution=480, framerate=25)
+                     resolution=480, framerate=20)
     # Compute y
     y = sqrt(t_sq * sin_2_p * (1 - x_sq / t_sq))
 
-    return [center[0] + x, center[1] + y]
+    return [x, y]
 
 
 # Function to mark an image at a specific point
 def mark_point(img, x, y, color, size):
-    pass
+    cv2.circle(img, (int(x), int(y)), size, color, thickness=-1)
+    return img
 
 
 # Function to log attitude data alongside video frames
@@ -66,7 +66,7 @@ def log_attitude_and_video(log_dir, vs, duration=30):
     # Create a VideoWriter object
     vw = VideoWriter(video_dir=new_dir_name,
                      video_file="video_log",
-                     resolution=480, framerate=10)
+                     resolution=480, framerate=20)
 
 
     # Print start message
@@ -88,13 +88,50 @@ def log_attitude_and_video(log_dir, vs, duration=30):
     vw.stop()
 
 
+# Function to add a marker indicating the ground point position to a video
+def overlay_gnd_pt(data_dir, video_file, attitude_file, new_video_file):
+
+    # Create VideoStreamer to read file
+    fs = VideoStreamer(src=data_dir + "/" + video_file)
+
+    # Create VideoWriter for modified video
+    vw = VideoWriter(video_dir=data_dir, video_file=new_video_file, framerate=20)
+
+    # Open attitude file
+    att_data = open(data_dir + "/" + attitude_file, "r")
+
+    # Construct the overlayed video
+    for line in att_data:
+
+        # Get the next data pair
+        rpy = [float(i) for i in line.split(",")]
+        frame = fs.read()
+
+        # Overlay the ground point
+        gp_offset = calc_gnd_pt_offset(degrees(rpy[0]), degrees(rpy[1]))
+        gp = [gp_offset[0] + 640 / 2.0, -gp_offset[1] + 480.0 / 2.0]
+        modified_frame = mark_point(frame, gp[0], gp[1], color=(255,0,0), size=5)
+
+        # Write modified frame to new video
+        vw.write(modified_frame)
+
+    # Close video writer
+    vw.stop()
+
+
 if __name__ == "__main__":
 
     # Create video stream
-    vs = VideoStreamer(use_pi=True, framerate=10)
+    vs = VideoStreamer(use_pi=True, framerate=20)
 
     # Log data
     log_attitude_and_video("/home/pi/attitude_logs", vs, duration=10)
+
+    # # Overlay ground point on video
+    # overlay_gnd_pt(data_dir="/home/jake/Desktop/dump/attitude_logs/log_1582267057",
+    #                video_file="video_log.avi",
+    #                attitude_file="attitude_log.txt",
+    #                new_video_file="overlayed.avi")
 
 
 
